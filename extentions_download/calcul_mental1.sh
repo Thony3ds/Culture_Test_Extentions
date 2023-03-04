70c6f1ed8dd8d8943bc2751e465787ec279ddd7c
Thony3ds <hiphop.antho@gmail.com>
2023-03-04 17:57:22 +0100

    update

--- extentions_download/calcul_mental1.sh
+++
@@ -1,3 +0,0 @@
-git clone https://github.com/Thony3ds/Culture_Test_Extentions
-wget https://github.com/Thony3ds/Culture_Test_Extentions/archive/master/extentions_download.zip
-unzip extentions_download.zip
---
+++ extentions_download/frensh/calcul_mental1.sh
@@ -0,0 +1,2 @@
+git clone https://github.com/Thony3ds/Culture_Test_Extentions
+wget https://github.com/Thony3ds/Culture_Test_Extentions/archive/master/extentions/frensh/calcul_mental1.zip
